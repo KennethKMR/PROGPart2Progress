4796e6c48fdedd3530d4d69b8a403e9bdb4b5d85
Kenneth Michael Remmitz <st10403075@rcconnect.edu.za>
2024-05-13 13:00:06 +0200

    I made some changes across both the Ingredient and Recipe class, both of these changes will be required to be reflected in the Program class. In the ingredient class, i added a override method. Meanwhile in the Recipe class i added the following: AddIngredients, AddStep, CalculateTotalCalories, ScaleRecipe(this was originally in the main class, but i altered it in order to make everything more seemless), and ClearRecipe, which also used to be in the main class.

--- PROG6221Part2/Ingredient.cs
+++ PROG6221Part2/Ingredient.cs
@@ -14,14 +14,20 @@ namespace PROG6221Part2
         public int Calories { get; set; } //Amount of calories
         public string FoodGroup { get; set; } //Type of food group
 
+        // Constructor for creating a new Ingredient
         public Ingredient(string name, double quantity, string unit, int calories, string foodgroup)
         {
-            Name = name;
-            Quantity = quantity;
-            Unit = unit;
-            Calories = calories;
-            FoodGroup = foodgroup;  
+            Name = name; // Initializes the Name property
+            Quantity = quantity; // Initializes the Quantity property
+            Unit = unit; // Initializes the Unit property
+            Calories = calories; // Initializes the Calories property
+            FoodGroup = foodgroup; // Initializes the FoodGroup property
 
+        }
+        public override string ToString()
+        {
+            // Constructs a string with the quantity, unit, name, and calories of the ingredient.
+            return $"{Quantity} {Unit} of {Name} ({Calories} calories)";
         }
     }
 }
--- PROG6221Part2/Recipe.cs
+++ PROG6221Part2/Recipe.cs
@@ -14,12 +14,45 @@ namespace PROG6221Part2
         public List<string> Steps { get; set; } //List of steps 
         public double[] OriginalQuantities { get; set; } //Array to store original quantities of ingredients
 
+        // Constructor for creating a new Recipe
         public Recipe(string name)
         {
-            Name = name;
-            Ingredients = new List<Ingredient>();
-            Steps = new List<string>();
+            Name = name;  // Initializes the Name property
+            Ingredients = new List<Ingredient>(); // Initializes the Ingredients list
+            Steps = new List<string>(); // Initializes the Steps list
 
         }
+        public void AddIngredient(Ingredient ingredient)
+        {
+            Ingredients.Add(ingredient); // Adds the given ingredient to the list of ingredients
+        }
+
+        public void AddStep(string step)
+        {
+            Steps.Add(step); // Adds the given step to the list of steps
+        }
+
+        public int CalculateTotalCalories()
+        {
+            int totalCalories = Ingredients.Sum(ingredient => ingredient.Calories); // Calculates the total calories of the recipe
+            return totalCalories;
+        }
+
+        public void ScaleRecipe(double factor)
+        {
+            foreach (var ingredient in Ingredients) // Scales the quantities of all ingredients in the recipe
+            {
+                ingredient.Quantity *= factor;
+            }
+        }
+
+        public void ClearRecipe()
+        {
+            Ingredients.Clear(); // Clears both the list of ingredients and the list of steps, basically it ends up clearing the entire recipe
+            Steps.Clear();
+        }
+
+
     }
 }
+   
